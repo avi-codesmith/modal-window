24502d2e093bb4b79c4691534464ddb09a00f51e
avi-codesmith <avi.codesmith@gmail.com>
2025-01-08 20:53:52 +0530

    Complete the Project

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 "use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+const OpenModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", OpenModal);
+}
+
+const CloseModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", CloseModal);
+overlay.addEventListener("click", CloseModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    CloseModal();
+  }
+});
